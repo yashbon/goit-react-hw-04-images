99369183eb353d4de2ee1e11021d973f9f4f9a88
Bohdan <yashbon@ukr.net>
2023-06-27 11:35:18 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,26 +10,34 @@ function Modal({ closeModal, largeImageURL }) {
     //     window.addEventListener('keydown', this.hendleKeyDown);
     // }
 
-    useEffect(() => {
-        window.addEventListener('keydown', hendleKeyDown);
-    });
+    // useEffect(() => {
+    //     window.addEventListener('keydown', hendleKeyDown);
+    // });
 
     // componentWillUnmount() {
     //     window.removeEventListener('keydown', this.hendleKeyDown);
     // }
 
     useEffect(() => {
+        function hendleKeyDown(event) {
+            if (event.code === 'Escape') {
+                // this.props.closeModal();
+                closeModal();
+            }
+        }
+
+        window.addEventListener('keydown', hendleKeyDown);
         return () => {
             window.removeEventListener('keydown', hendleKeyDown);
         };
-    });
+    }, [closeModal]);
 
-    function hendleKeyDown(event) {
-        if (event.code === 'Escape') {
-            // this.props.closeModal();
-            closeModal();
-        }
-    }
+    // function hendleKeyDown(event) {
+    //     if (event.code === 'Escape') {
+    //         // this.props.closeModal();
+    //         closeModal();
+    //     }
+    // }
 
     function handleBackdropClick(event) {
         if (event.target === event.currentTarget) {
